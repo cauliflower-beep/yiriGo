e23924b544f6f20ae8f920ec569ef03dc72f0a18
lsx <642545320@qq.com>
2022-09-20 10:36:46 +0800

    9.19

---
+++ 3rd_pkg/cobra/demo.go
@@ -0,0 +1,7 @@
+package main
+
+/*
+	cobra既是一个用来创建强大的现代CLI(Command Line Interface)命令行的golang库，也是一个生成程序应用和命令行文件的程序。
+	使用 Linux 系统的大部分用户更习惯采用 CLI 进行人机交互。在Win系统诞生以前，它的前身DOS系统也是字符界面的 CLI 人机交互系统;
+	而目前Win系统的程序大部分是图形界面GUI(Graphical User Interface)程序，但由于CLI的一些特点和独特的优势，它还不能被完全抛弃。
+*/
--- demo/part1.go
+++ demo/part1.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 var p = newPerson()
 
 func newPerson() *person {
@@ -7,3 +12,12 @@ func newPerson() *person {
 }
 
 type person struct{}
+
+func main() {
+	rsp, err := http.Get("https://www.baidu.com")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rsp.Body.Close()
+	fmt.Println(rsp.Body)
+}
