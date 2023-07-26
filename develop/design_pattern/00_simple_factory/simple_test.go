11ed55be1453f81a0a86c9d5d798ddbf72b6070a
lsx <642545320@qq.com>
2023-07-26 10:14:00 +0800

    simple_factory

---
+++ develop/design_pattern/00_simple_factory/simple_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestType1(t *testing.T) {
+	api := NewAPI(1)
+	s := api.Say("Tom")
+	if s != "hi, Tom" {
+		t.Fatal("Type1 test fail")
+	}
+}
+
+func TestType2(t *testing.T) {
+	api := NewAPI(2)
+	s := api.Say("Tom")
+	if s != "Hello, Tom" {
+		t.Fatal("Type2 test fail")
+	}
+}
