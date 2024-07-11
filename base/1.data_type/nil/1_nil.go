eff46e0e28b684f85c3f09b7301344449e008692
lsx_hp_win <642545320@qq.com>
2024-07-11 17:40:11 +0800

    nil说明

---
+++ base/1.data_type/nil/1_nil.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func getFunc(flag string) func(x, y int) int {
+	if flag == "add" {
+		return func(x, y int) int {
+			return x + y
+		}
+	} else if flag == "minus" {
+		return func(x, y int) int {
+			return x - y
+		}
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	fmt.Println(getFunc("add")(1, 2))
+
+	fmt.Println(getFunc("minus")(1, 2))
+
+	fmt.Println(getFunc("multiply")(1, 2))
+}
---
+++ base/1.data_type/nil/_nil.md
@@ -0,0 +1,3 @@
+
+
+`nil`可以用作`interface`、`function`、`pointer`、`map`、`slice`和`channel`的"空值"。
